6c2d91407da2590ea9e391e859bdc46f684c58c5
ptrdyp <alviany.prameswari@gmail.com>
2023-10-25 14:48:56 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/myunlimitedquotes/ui/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.dicoding.myunlimitedquotes.ui
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.myunlimitedquotes.adapter.QuoteListAdapter
+import com.dicoding.myunlimitedquotes.databinding.ActivityMainBinding
+import com.dicoding.myunlimitedquotes.ui.MainViewModel
+import com.dicoding.myunlimitedquotes.ui.ViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels {
+        ViewModelFactory(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvQuote.layoutManager = LinearLayoutManager(this)
+
+        getData()
+    }
+
+    private fun getData() {
+        val adapter = QuoteListAdapter()
+        binding.rvQuote.adapter = adapter
+        mainViewModel.quote.observe(this) {
+            adapter.submitData(lifecycle, it)
+        }
+    }
+}
